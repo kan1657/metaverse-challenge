91499dfbe258f135b78be6784e562d5f87e1e5d3
Kan <kan1657@hotmail.com>
2021-12-30 17:31:28 +0700

    Finish login component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
 import Login from "../components/Login";
+import { useMoralis } from "react-moralis";
 
 export default function Home() {
+  const { isAuthenticated, logout } = useMoralis();
+  if (!isAuthenticated) return <Login />;
+
   return (
-    <div className="">
+    <div className="h-screen">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Login />
+      <h1>Welcome to the app</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
